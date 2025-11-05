f4e662ddf30a45d78164a6d0d6f8b64ac8558d5d
Stump-Dude <puchkinyehor@gmail.com>
2025-11-05 13:38:09 +0000

    ,

--- DataStructureList04/DataStructureList04/Program.cs
+++ DataStructureList04/DataStructureList04/Program.cs
@@ -1,1 +1,69 @@
-﻿
+﻿using System.Collections;
+
+public enum Type
+{
+    Weapon,
+    Armor,
+    Potion,
+    Scroll
+}
+
+public enum Rarity
+{
+    Common,
+    Uncommon,
+    Rare,
+    Epic,
+    Legendary
+}
+
+public class Item
+{
+    public string name { get; }
+    public Type type { get; set; }
+    public Rarity rarity { get; set; }
+    public int numberSlots { get; }
+    public bool uniqueUse { get; }
+
+
+    Item(string name, Type type, Rarity rarity, int numberSlots, bool uniqueUse)
+    {
+        this.name = name;
+        this.type = type;
+        this.rarity = rarity;
+        this.numberSlots = numberSlots;
+        this.uniqueUse = uniqueUse;
+    }
+}
+public class Inventory
+{
+    Hashtable inventory = new Hashtable();
+
+    int slotsOccupied;
+    int maximumSLots;
+
+    Item item = new Item("Sword", Type.Weapon, Rarity.Common, 1, false);
+    bool InsertItem(Item item)
+    {
+        return true;
+    }
+    Item Remove(Item item)
+    {
+        return;
+    }
+
+    int NumberOfItems()
+    {
+        return 0;
+    }
+
+    int NumberOfSlots()
+    {
+        return 0;
+    }
+
+    CleanInventory()
+    {
+
+    }
+}
--- DataStructureList04/DataStructureList04/obj/Release/net9.0/DataStructureList04.AssemblyInfo.cs
+++ DataStructureList04/DataStructureList04/obj/Release/net9.0/DataStructureList04.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataStructureList04")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad4cd75d8994ff9af4ac7848b097a70fc5830266")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataStructureList04")]
